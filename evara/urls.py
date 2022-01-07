49a092a0b3b4e99d64df1ebfb2fa0f32028f8c94
a4Aby <a4abyy@gmail.com>
2022-01-07 16:05:41 +0530

    super user title

--- evara/urls.py
+++ evara/urls.py
@@ -19,6 +19,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header  =  "Evara superuser"  
+admin.site.site_title  =  "Evara superuser"
+admin.site.index_title  =  "Evara superuser"
+
 urlpatterns = [
     path('', include('home.urls')),
     path('', include('administration.urls')),
